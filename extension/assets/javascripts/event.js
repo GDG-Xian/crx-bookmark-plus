8f8b400a0ca3527cfd0e756cbd3a2c6aeab0616a
greatghoul <greatghoul@gmail.com>
2015-03-10 21:02:40 +0800

    show different icon on page bookmarked status

--- extension/assets/javascripts/event.js
+++ extension/assets/javascripts/event.js
@@ -1,7 +1,23 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  chrome.pageAction.show(tabId);
-});
+function showPageIcon(tab) {
+  chrome.bookmarks.search(tab.url, function(results) {
+    var details = { tabId: tab.id };
+    if (results.length) {
+      details.path = 'assets/images/icon_128.png';
+    } else {
+      details.path = 'assets/images/icon_128_gray.png';
+    }
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.pageAction.show(activeInfo.tabId);
-});
+    chrome.pageAction.setIcon(details)
+    chrome.pageAction.show(tab.id);
+  });
+}
+
+function updateActiveTabIcon() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { showPageIcon(tabs[0]);
+  });
+}
+
+chrome.tabs.onUpdated.addListener(updateActiveTabIcon);
+chrome.tabs.onActivated.addListener(updateActiveTabIcon);
+chrome.bookmarks.onCreated.addListener(updateActiveTabIcon);
+chrome.bookmarks.onRemoved.addListener(updateActiveTabIcon);
